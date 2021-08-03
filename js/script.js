f5b731bd55d81f77b96e9db86453e80d701bc25a
AndryDevelopeer <76574661+AndryDevelopeer@users.noreply.github.com>
2021-08-03 22:39:29 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let click = 0;// для отслеживания кликов
 function show() {
     if (click === 0) {
         document.getElementById('about_text').style.display = 'block';
-        // ищем в документе блок с ид 'about_text', меняем свойство на 'block'
+        //в документе блок с ид 'about_text', меняем свойство на 'block'
         click++;//инкремент
     } else {
         document.getElementById('about_text').style.display = 'none';
-        // ищем в документе блок с ид 'about_text', меняем свойство на 'none'
+        //в документе блок с ид 'about_text', меняем свойство на 'none'
         click--;//декремент
     }
 }
